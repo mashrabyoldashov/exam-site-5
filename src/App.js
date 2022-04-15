37bcac61327e96c5134eb474ca7de829ed2d3fa6
yoldashovmashrab <yoldashovmashrab6003@gmail.com>
2022-04-15 04:43:30 -0700

    main

--- src/App.js
+++ src/App.js
@@ -12,26 +12,29 @@ import Login from './pages/login';
 import Public from './Routes/Public';
 import Private from './Routes/Private';
 import Admin from './pages/admin';
+import Main from './components/main/main';
 
 
 function App() {
   return (
     <div className='App'>
       <Header />
+        <Main>
           <Routes>
-            <Route path='/' element={<Public />}>
-                <Route path='/korzinka' element={<Korzinka />}/>
-                <Route path='/selected/:id' element={<Selected/>}/>
-                <Route path='/kontak' element={<Kontak/>}/>
-                <Route path='/finish' element={<Finish/>}/>
-                <Route path='/like' element={<Like />}/>
-                <Route path='/' element={<OnlineShop/>}/>
-            </Route>
-            <Route path='/' element={<Private/>}>
-                <Route path='/admin' element={<Admin/>}/>
-            </Route>
-            
+              <Route path='/' element={<Public />}>
+                  <Route path='/korzinka' element={<Korzinka />}/>
+                  <Route path='/selected/:id' element={<Selected/>}/>
+                  <Route path='/kontak' element={<Kontak/>}/>
+                  <Route path='/finish' element={<Finish/>}/>
+                  <Route path='/like' element={<Like />}/>
+                  <Route path='/' element={<OnlineShop/>}/>
+              </Route>
+              <Route path='/' element={<Private/>}>
+                  <Route path='/admin' element={<Admin/>}/>
+              </Route>
+              
           </Routes>
+        </Main>
       <Footer/>
     </div>
   );
